6d856a36f5715c379bf93f3495c9e96e75cf3f35
Avery Mack <avery.mack@ocsbstudent.ca>
2018-03-06 11:41:59 -0500

    changes

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 -----------------------------------------------------------------------------------------
---
+-- Title: TouchAndDrag
+-- Name: Avery Mack
+-- Course: ICS2O/3C
+-- This program displays images that can be dragged around on the screen.
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
