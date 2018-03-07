63f6fee8d1fc7391ff732588e69c40e277f32737
Avery Mack <avery.mack@ocsbstudent.ca>
2018-03-07 11:05:05 -0500

    changes

--- main.lua
+++ main.lua
@@ -8,3 +8,51 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- local variables. I am still trying to get the x-value to be set properly
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+local catGirl = display.newImageRect("Images/girl4.png", 150, 150)
+local catGirlWidth = catGirl.width
+local catGirlHeight = catGirl.height
+
+local starGirl = display.newImageRect("Images/starGirl.png", 150, 150)
+local starGirlWidth = starGirl.width
+local starGirlHeight = starGirl.height
+
+-- my boolean variables to keep track of which object I touched first
+local alreadyTouchedCatGirl = false
+local alreadyTouchedStarGirl = false 
+
+-- set the initial x and y position of myImage
+catGirl.x = 400
+catGirl.y = 500
+
+starGirl.x = 300
+starGirl.y = 200
+
+-- Function: BlueGirlListener
+-- Input: touch listener
+-- Output: none
+-- Description: when blue girl is touched, move her
+local function BlueGirlListener(touch)
+
+	if (touch.phase == "began") then 
+		if (alreadyTouchedCatGirl == false) then
+			alreadyTouchedStarGirl = true
+		end 
+	end
+
+	if ( (touch.phase == "moved") and (alreadyTouchedStarGirl == true) ) then
+		starGirl.x = touch.x
+		starGirl.y = touch.y
+	end
+
+	if 
+
+
+
+
+
+
+
+
